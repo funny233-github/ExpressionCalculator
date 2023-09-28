cd58795de2cc2cf2881090f908a34039b06c9bb2
funny_233 <83470506+funny233-github@users.noreply.github.com>
2023-09-28 11:28:45 +0800

    finish interpreter class

--- calculate.py
+++ calculate.py
@@ -1,13 +1,8 @@
 import re
 from enum import Enum,unique
 
-class Lexer:
-    tokens = None
-    varibles:dict[str,float|int] = {
-        "TEST_VARIBLE":114514,
-        "PI":3.1415926,
-    }
-    operators = {
+class operators:
+    operator = {
         "LParentheses":4,
         "RParentheses":4,
 
@@ -24,6 +19,13 @@ class Lexer:
         "add":0,
         "sub":0,
 
+    }   
+
+class Lexer:
+    tokens:list = []
+    varibles:dict[str,float|int] = {
+        "TEST_VARIBLE":114514,
+        "PI":3.1415926,
     }
     def stringValueToRealValue(self,string:str):
         isVarible = string in self.varibles
@@ -75,18 +77,90 @@ class Lexer:
 
     def __init__(self,exp:str):
         self.participle(exp)
-class interpreter:
-    operatorStack = []
-    valueStack = []
+class interpreter(operators):
+    operatorStack:list = []
+    valueStack:list = []
 
-    def test(self,tokens:list):
+    def ifTokenLowerThanBefore(self,token):
+        if len(self.operatorStack) == 0 or type(token) != str:
+            return False
+        if token == "(" or self.operatorStack[-1] == "(":#))
+            return False
+        if token == ")":
+            return True
+        if self.operatorStack[-1] == ")":
+            return True
+        if self.operator[token] < self.operator[self.operatorStack[-1]]:
+            return True
+        return False
+    def calculate(self,operator):
+        if operator == "pos":
+            result = self.valueStack.pop()
+            self.valueStack.append(result)
+        if operator == "neg":
+            result = -1 * self.valueStack.pop()
+            self.valueStack.append(result)
+        if operator == "add":
+            result = self.valueStack.pop() + self.valueStack.pop()
+            self.valueStack.append(result)
+        if operator == "sub":
+            right = self.valueStack.pop()
+            left = self.valueStack.pop()
+            result = left - right
+            self.valueStack.append(result)
+        if operator == "mul":
+            result = self.valueStack.pop() * self.valueStack.pop()
+            self.valueStack.append(result)
+        if operator == "truediv":
+            right = self.valueStack.pop()
+            left = self.valueStack.pop()
+            result = left / right
+            self.valueStack.append(result)
+        if operator == "floordiv":
+            right = self.valueStack.pop()
+            left = self.valueStack.pop()
+            result = left // right
+            self.valueStack.append(result)
+        if operator == "mod":
+            right = self.valueStack.pop()
+            left = self.valueStack.pop()
+            result = left % right
+            self.valueStack.append(result)
+        if operator == "pow":
+            right = self.valueStack.pop()
+            left = self.valueStack.pop()
+            result = left ** right
+            self.valueStack.append(result)
+    def stackOut(self,token):
+        while self.ifTokenLowerThanBefore(token):
+            operator = self.operatorStack.pop()
+            if operator == "(":#)
+                continue
+            self.calculate(operator)
+        if token == ")":
+            self.operatorStack.pop()
+            return
+        self.operatorStack.append(token)
         return
 
+    def stackIn(self,tokens:list):
+        for token in tokens:
+            isValue = type(token) != str
+            if isValue:
+                self.valueStack.append(token)
+            elif self.ifTokenLowerThanBefore(token):
+                self.stackOut(token)
+            else:
+                self.operatorStack.append(token)
+        while len(self.operatorStack) != 0:
+            operator = self.operatorStack.pop()
+            self.calculate(operator)
+ 
     def __init__(self,tokens:list):
-        self.tokens = tokens
+        self.stackIn(tokens)
+        self.result = self.valueStack[0]
 
-
-class expressionCalculator():
-    operatorStack = []
-    valueStack = []
+class expressionCalculator:
+    def __init__(self,exp:str):
+        self.result = interpreter(Lexer(exp).tokens).result
 test = r"[a-zA-Z_]+\(|[0-9\.a-zA-Z_]+|\*\*|\/\/|[^0-9\.a-zA-Z_ ]{1}"
